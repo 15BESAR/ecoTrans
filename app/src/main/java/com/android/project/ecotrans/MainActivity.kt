dd971214d44d222b71fc258316577736754cd60f
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-16 16:19:15 +0700

    Basic Structure for login register v2

--- app/src/main/java/com/android/project/ecotrans/MainActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.android.project.ecotrans
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
