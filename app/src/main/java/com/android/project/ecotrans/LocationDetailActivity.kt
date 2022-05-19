c37dc386dc4a2354643f20567ef4467618c526c5
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-19 15:15:54 +0700

    locationDetail created

---
+++ app/src/main/java/com/android/project/ecotrans/LocationDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.android.project.ecotrans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_detail)
+    }
+}
