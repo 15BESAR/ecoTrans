8a5b7422305c7fa9c66fd7f05b125947e923f25f
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-20 17:18:05 +0700

    profileActivity + ViewModel prepared

--- app/src/main/java/com/android/project/ecotrans/ProfileActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/ProfileActivity.kt
@@ -1,11 +1,67 @@
 package com.android.project.ecotrans
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.android.project.ecotrans.databinding.ActivityMainBinding
+import com.android.project.ecotrans.databinding.ActivityProfileBinding
+import com.android.project.ecotrans.model.UserPreference
+import com.android.project.ecotrans.view_model.MainViewModel
+import com.android.project.ecotrans.view_model.ProfileViewModel
+import com.android.project.ecotrans.view_model.ViewModelFactory
+
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var profileViewModel: ProfileViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        setupView()
+        setupViewModel()
+        setupAction()
+        setupAnimation()
+    }
+
+    private fun setupAnimation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupAction() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupViewModel() {
+        profileViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore), this)
+        )[ProfileViewModel::class.java]
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+//            binding.mprogressBar.visibility = View.VISIBLE
+        } else {
+//            binding.mprogressBar.visibility = View.GONE
+        }
+    }
+
+    private fun showError(isError: Boolean){
+        if (isError){
+            Toast.makeText(this@ProfileActivity, "ERROR", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/android/project/ecotrans/view_model/ProfileViewModel.kt
@@ -0,0 +1,67 @@
+package com.android.project.ecotrans.view_model
+
+import androidx.lifecycle.*
+import com.android.project.ecotrans.model.UserModel
+import com.android.project.ecotrans.model.UserPreference
+import kotlinx.coroutines.launch
+
+class ProfileViewModel(private val pref: UserPreference) : ViewModel() {
+
+    private var _isLoading = MutableLiveData<Boolean>()
+    var isLoading: LiveData<Boolean> = _isLoading
+
+    private var _isError = MutableLiveData<Boolean>()
+    var isError: LiveData<Boolean> = _isError
+
+    private var _errorMessage = MutableLiveData<String>()
+    var errorMessage: LiveData<String> = _errorMessage
+
+    fun getUser(): LiveData<UserModel> {
+        return pref.getUser().asLiveData()
+    }
+
+    fun saveUser(user: UserModel) {
+        viewModelScope.launch {
+            pref.saveUser(user)
+        }
+    }
+
+    init {
+    }
+
+    fun postUpdate(email: String, password: String) {
+//        _isLoading.value = true
+//        _isError.value = false
+//        var userModel: UserModel
+//        var client = ApiConfig.getApiService().login(email, password)
+//        client.enqueue(object : Callback<PostResponseLogin> {
+//            override fun onResponse(
+//                call: Call<PostResponseLogin>,
+//                response: retrofit2.Response<PostResponseLogin>
+//            ) {
+//                _isLoading.value = false
+//                if (response.isSuccessful) {
+//                    var id = response.body()?.loginResult?.userId as String
+//                    var name = response.body()?.loginResult?.name as String
+//                    var isLogin = true
+//                    var token = response.body()?.loginResult?.token as String
+//                    userModel = UserModel(id, name, isLogin, token)
+//
+//                    _errorMessage.value = "login " + response.body()?.message as String
+//                    saveUser(userModel)
+//                } else {
+//                    Log.e("MainActivity", "onFailure: ${response.message()}")
+//
+//                    _errorMessage.value = "Wrong Password or Email"
+//                    _isError.value = true
+//                }
+//            }
+//            override fun onFailure(call: Call<PostResponseLogin>, t: Throwable) {
+//                _isLoading.value = false
+//                _isError.value = true
+//                _errorMessage.value = t.message as String
+//                Log.e("MainActivity", "onFailure: ${t.message}")
+//            }
+//        })
+    }
+}
