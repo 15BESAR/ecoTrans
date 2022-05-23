0b06623139157e190fe4685103a37d7332940f0d
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-23 20:14:30 +0700

    update LocationDetailActivity.kt

--- app/src/main/java/com/android/project/ecotrans/LocationDetailActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/LocationDetailActivity.kt
@@ -7,5 +7,16 @@ class LocationDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_detail)
+
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
     }
 }
