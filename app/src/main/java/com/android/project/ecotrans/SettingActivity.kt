7413a18e17657e13c07cbd367f4d7db66e09bbf2
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-23 20:15:47 +0700

    update SettingActivity.kt

--- app/src/main/java/com/android/project/ecotrans/SettingActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/SettingActivity.kt
@@ -7,5 +7,26 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        setupView()
+        setupViewModel()
+        setupAction()
+        setupAnimation()
+    }
+
+    private fun setupAnimation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupAction() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
     }
 }
