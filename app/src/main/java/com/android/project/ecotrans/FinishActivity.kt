5870a75fb3b3e36e3fc81c0af6f274618d128333
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-23 20:12:43 +0700

    update FinishActivity.kt

--- app/src/main/java/com/android/project/ecotrans/FinishActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/FinishActivity.kt
@@ -7,5 +7,16 @@ class FinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
     }
 }
