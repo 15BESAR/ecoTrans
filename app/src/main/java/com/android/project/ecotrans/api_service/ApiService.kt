5d32796554a621b887c6c7321ce8b4934d6a576e
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-24 16:21:14 +0700

    temp api using old project api

--- app/src/main/java/com/android/project/ecotrans/api_service/ApiService.kt
+++ app/src/main/java/com/android/project/ecotrans/api_service/ApiService.kt
@@ -1,46 +1,44 @@
 package com.android.project.ecotrans.api_service
 
+import com.android.project.ecotrans.response.PostResponseLogin
+import com.android.project.ecotrans.response.PostResponseRegister
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
 
-    @FormUrlEncoded
-    @POST("register")
-    fun register(
-        @Field("username") username: String,
-        @Field("password") password: String,
-        @Field("email") email: String,
-        @Field("firstName") firstName: String,
-        @Field("lastName") lastName: String,
-        @Field("birthDate") birthDate: String
-    ): Call<ResponseRegister>
+//    @FormUrlEncoded
+//    @POST("register")
+//    fun register(
+//        @Field("username") username: String,
+//        @Field("password") password: String,
+//        @Field("email") email: String,
+//        @Field("firstName") firstName: String,
+//        @Field("lastName") lastName: String,
+//        @Field("birthDate") birthDate: String
+//    ): Call<ResponseRegister>
+//
+//    @FormUrlEncoded
+//    @POST("login")
+//    fun login(
+//        @Field("username") username: String,
+//        @Field("password") password: String
+//    ): Call<ResponseLogin>
+
 
     @FormUrlEncoded
     @POST("login")
     fun login(
-        @Field("username") username: String,
+        @Field("email") email: String,
         @Field("password") password: String
-    ): Call<ResponseLogin>
+    ): Call<PostResponseLogin>
 
-//    @GET("search/users")
-//    fun searchUser(
-//        @Query("q") q: String
-//    ): Call<ResponseQ>
-//
-//    @GET("users/{username}")
-//    fun detailUser(
-//        @Path("username") username: String
-//    ): Call<ResponseU>
-//
-//    @GET("users/{username}/followers")
-//    fun followerUser(
-//        @Path("username") username: String
-//    ): Call<List<ResponseFItem>>
-//
-//    @GET("users/{username}/following")
-//    fun followingUser(
-//        @Path("username") username: String
-//    ): Call<List<ResponseFItem>>
+    @FormUrlEncoded
+    @POST("register")
+    fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<PostResponseRegister>
 
 }
