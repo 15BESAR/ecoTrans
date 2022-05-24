ea8b297aa616a22ba4fe9d8e10bee794b93fd4c7
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-24 14:08:23 +0700

    create PurchaseActivity for buying voucher with rewardCoins

---
+++ app/src/main/java/com/android/project/ecotrans/PurchaseActivity.kt
@@ -0,0 +1,32 @@
+package com.android.project.ecotrans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PurchaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase)
+
+        setupView()
+        setupViewModel()
+        setupAction()
+        setupAnimation()
+    }
+
+    private fun setupAnimation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupAction() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
+    }
+}
