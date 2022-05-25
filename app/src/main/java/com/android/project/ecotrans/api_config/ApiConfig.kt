82409c29908bf77eaeebad961c381d6b8249909a
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-25 12:43:30 +0700

    update the correct api base URL

--- app/src/main/java/com/android/project/ecotrans/api_config/ApiConfig.kt
+++ app/src/main/java/com/android/project/ecotrans/api_config/ApiConfig.kt
@@ -20,7 +20,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("https://story-api.dicoding.dev/v1/")
+                .baseUrl("http://34.101.186.107/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
