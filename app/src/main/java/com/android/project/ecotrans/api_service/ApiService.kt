b34be27e1d75ebecd5a4fa747d5860bd36fd7a1e
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-25 12:43:14 +0700

    update login register and search location api service

--- app/src/main/java/com/android/project/ecotrans/api_service/ApiService.kt
+++ app/src/main/java/com/android/project/ecotrans/api_service/ApiService.kt
@@ -1,44 +1,62 @@
 package com.android.project.ecotrans.api_service
 
-import com.android.project.ecotrans.response.PostResponseLogin
-import com.android.project.ecotrans.response.PostResponseRegister
+import com.android.project.ecotrans.model.User
+import com.android.project.ecotrans.response.ResponseAutoComplete
+import com.android.project.ecotrans.response.ResponseLogin
+import com.android.project.ecotrans.response.ResponseRegister
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
 
+    @Headers("Content-Type: application/json")
 //    @FormUrlEncoded
-//    @POST("register")
-//    fun register(
+    @POST("register")
+    fun register(
 //        @Field("username") username: String,
 //        @Field("password") password: String,
 //        @Field("email") email: String,
 //        @Field("firstName") firstName: String,
 //        @Field("lastName") lastName: String,
 //        @Field("birthDate") birthDate: String
-//    ): Call<ResponseRegister>
-//
+        @Body requestBody: RequestBody
+    ): Call<ResponseRegister>
+
+    @Headers("Content-Type: application/json")
+//    @FormUrlEncoded
+    @POST("login")
+    fun login(
+//        @Field("username") username: String,
+//        @Field("password") password: String
+        @Body requestBody: RequestBody
+    ): Call<ResponseLogin>
+
+    @GET("user/{id}")
+    fun user(
+        @Path("id") id: String
+    ): Call<User>
+
+
 //    @FormUrlEncoded
 //    @POST("login")
 //    fun login(
-//        @Field("username") username: String,
+//        @Field("email") email: String,
 //        @Field("password") password: String
-//    ): Call<ResponseLogin>
+//    ): Call<PostResponseLogin>
+//
+//    @FormUrlEncoded
+//    @POST("register")
+//    fun register(
+//        @Field("name") name: String,
+//        @Field("email") email: String,
+//        @Field("password") password: String
+//    ): Call<PostResponseRegister>
 
-
-    @FormUrlEncoded
-    @POST("login")
-    fun login(
-        @Field("email") email: String,
-        @Field("password") password: String
-    ): Call<PostResponseLogin>
-
-    @FormUrlEncoded
-    @POST("register")
-    fun register(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field("password") password: String
-    ): Call<PostResponseRegister>
+    @Headers("Content-Type: application/json")
+    @GET("autocomplete")
+    fun searchLocation(
+        @Body requestBody: RequestBody
+    ): Call<ResponseAutoComplete>
 
 }
