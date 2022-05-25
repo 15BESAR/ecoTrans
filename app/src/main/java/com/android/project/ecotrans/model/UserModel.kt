974aa674bdb0a8b2b2ee62356502ef083af94c2e
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-25 12:42:04 +0700

    update the correct userModel for the apps

--- app/src/main/java/com/android/project/ecotrans/model/UserModel.kt
+++ app/src/main/java/com/android/project/ecotrans/model/UserModel.kt
@@ -2,7 +2,7 @@ package com.android.project.ecotrans.model
 
 data class UserModel(
     val id: String,
-    val name: String,
+    val username: String,
     val isLogin: Boolean,
     val token: String
 )
