c0f5ae5ce4350384c414d082f8c820fdc7926829
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-26 17:25:32 +0700

    small update to prepare PurchaseActivity.kt

--- app/src/main/java/com/android/project/ecotrans/PurchaseActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/PurchaseActivity.kt
@@ -1,29 +1,57 @@
 package com.android.project.ecotrans
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.android.project.ecotrans.databinding.ActivityPurchaseBinding
+import com.android.project.ecotrans.model.UserPreference
+import com.android.project.ecotrans.view_model.PurchaseViewModel
+import com.android.project.ecotrans.view_model.ViewModelFactory
+
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class PurchaseActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPurchaseBinding
+    private lateinit var purchaseViewModel: PurchaseViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchase)
 
+        val isDetailed:Boolean = intent.getBooleanExtra("isDetailed", false)
+
         setupView()
-        setupViewModel()
-        setupAction()
-        setupAnimation()
+        setupViewModel(isDetailed)
+//        setupAction()
+//        setupAnimation()
     }
 
-    private fun setupAnimation() {
-        TODO("Not yet implemented")
-    }
+//    private fun setupAnimation() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    private fun setupAction() {
+//        TODO("Not yet implemented")
+//    }
 
-    private fun setupAction() {
-        TODO("Not yet implemented")
-    }
+    private fun setupViewModel(isDetailed: Boolean) {
+        purchaseViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore), this)
+        )[PurchaseViewModel::class.java]
 
-    private fun setupViewModel() {
-        TODO("Not yet implemented")
+        purchaseViewModel.getUser().observe(this){
+            if (!isDetailed){
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
     }
 
     private fun setupView() {
