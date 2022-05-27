e5ccaabe143bb0f42325624a217fcd3c0f492744
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-27 18:02:56 +0700

    add finishViewModel to factory

--- app/src/main/java/com/android/project/ecotrans/view_model/ViewModelFactory.kt
+++ app/src/main/java/com/android/project/ecotrans/view_model/ViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.android.project.ecotrans.view_model
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.android.project.ecotrans.FinishActivity
 import com.android.project.ecotrans.model.UserPreference
 
 
@@ -29,20 +30,23 @@ class ViewModelFactory(private val pref: UserPreference, private val context: Co
             modelClass.isAssignableFrom(LocationDetailViewModel::class.java) -> {
                 LocationDetailViewModel(pref) as T
             }
+            modelClass.isAssignableFrom(FinishViewModel::class.java) -> {
+                FinishViewModel(pref) as T
+            }
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
     }
 }
 
-class ViewMainModelFactory(private val pref: UserPreference, private val context: Context, private val token: String) : ViewModelProvider.NewInstanceFactory() {
-
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return when {
-//            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
-//                MainViewModel(pref, Injection.provideRepository(context), token) as T
-//            }
-            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
-        }
-    }
-}
+//class ViewMainModelFactory(private val pref: UserPreference, private val context: Context, private val token: String) : ViewModelProvider.NewInstanceFactory() {
+//
+//    @Suppress("UNCHECKED_CAST")
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        return when {
+////            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+////                MainViewModel(pref, Injection.provideRepository(context), token) as T
+////            }
+//            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+//        }
+//    }
+//}
