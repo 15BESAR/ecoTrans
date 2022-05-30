7df8985bc787315b3ba0d1ab1e608a39ed949337
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-05-30 15:40:42 +0700

    add bought fiture to the app

---
+++ app/src/main/java/com/android/project/ecotrans/BoughtActivity.kt
@@ -0,0 +1,131 @@
+package com.android.project.ecotrans
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.project.ecotrans.databinding.ActivityBoughtBinding
+import com.android.project.ecotrans.databinding.ActivityMainBinding
+import com.android.project.ecotrans.databinding.ActivityPurchaseBinding
+import com.android.project.ecotrans.model.UserPreference
+import com.android.project.ecotrans.response.ResponseVoucher
+import com.android.project.ecotrans.view_model.BoughtViewModel
+import com.android.project.ecotrans.view_model.PurchaseViewModel
+import com.android.project.ecotrans.view_model.ViewModelFactory
+
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+class BoughtActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBoughtBinding
+    private lateinit var boughtViewModel: BoughtViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBoughtBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val isDetailed:Boolean = intent.getBooleanExtra("isDetailed", false)
+
+        setupViewModel(isDetailed)
+        setupView()
+        setupAction()
+//        setupAnimation()
+    }
+
+    private fun setupAction() {
+        //
+    }
+
+//    private fun setupAnimation() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    private fun setupAction() {
+//        TODO("Not yet implemented")
+//    }
+
+    private fun setupViewModel(isDetailed: Boolean) {
+        boughtViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore), this)
+        )[BoughtViewModel::class.java]
+
+        boughtViewModel.errorMessage.observe(this){
+            showErrorMessage(it)
+        }
+    }
+
+    private fun setupView() {
+        supportActionBar?.hide()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewBoughtVoucherlist.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.recyclerViewBoughtVoucherlist.addItemDecoration(itemDecoration)
+        setupVoucherList()
+    }
+
+    private fun setupVoucherList(){
+        val listVoucher = ArrayList<ResponseVoucher>()
+        var voucher : ResponseVoucher
+//        for (item in items!!){
+//            user = User()
+//
+//            user.id = item?.id
+//            user.login = item?.login
+//            user.avatar_url = item?.avatarUrl
+//            user.followers_url = item?.followersUrl
+//            user.following_url = item?.followingUrl
+//            user.type = item?.type
+//            user.url = item?.url
+//
+//            listItem.add(user)
+//        }
+
+        voucher = ResponseVoucher()
+        voucher.category = "GoFood"
+        voucher.partnerName = "GOJEK"
+        voucher.price = 500
+        voucher.stock = 10
+        voucher.voucherName = "Diskon 50% hingga 25RB"
+        listVoucher.add(voucher)
+
+        voucher = ResponseVoucher()
+        voucher.category = "Furniture"
+        voucher.partnerName = "Tokopedia"
+        voucher.price = 1000
+        voucher.stock = 10
+        voucher.voucherName = "Diskon 5% hingga 100RB"
+        listVoucher.add(voucher)
+
+        voucher = ResponseVoucher()
+        voucher.category = "GoSend"
+        voucher.partnerName = "GOJEK"
+        voucher.price = 200
+        voucher.stock = 10
+        voucher.voucherName = "Gratis Ongkir hingga 10RB"
+        listVoucher.add(voucher)
+
+        val adapter = BoughtVoucherListAdapter()
+        adapter.setContext(this)
+        adapter.setListVoucher(listVoucher)
+        binding.recyclerViewBoughtVoucherlist.adapter = adapter
+    }
+
+//    private fun buyVoucher(data: ResponseVoucher) {
+//        //
+//    }
+
+    private fun showErrorMessage(errorMessage: String){
+        Toast.makeText(this@BoughtActivity, errorMessage.toString(), Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/android/project/ecotrans/BoughtVoucherListAdapter.kt
@@ -0,0 +1,61 @@
+package com.android.project.ecotrans
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.recyclerview.widget.RecyclerView
+import com.android.project.ecotrans.databinding.ActivityBoughtVoucherlistItemBinding
+import com.android.project.ecotrans.response.ResponseVoucher
+import com.android.project.ecotrans.view_model.BoughtViewModel
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+class BoughtVoucherListAdapter() : RecyclerView.Adapter<BoughtVoucherListAdapter.BoughtVoucherListHolder>(){
+    private lateinit var context: Context
+
+    fun setContext(context: Context){
+        this.context = context
+    }
+
+    private val listVoucher: ArrayList<ResponseVoucher> = ArrayList()
+    fun setListVoucher(listVoucher: List<ResponseVoucher>){
+        this.listVoucher.addAll(listVoucher)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoughtVoucherListHolder {
+        val binding = ActivityBoughtVoucherlistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BoughtVoucherListHolder(binding, context)
+    }
+
+    override fun onBindViewHolder(holder: BoughtVoucherListHolder, position: Int) {
+//        holder.itemView.setOnClickListener {
+//            onItemClickCallback.onItemClicked(listVoucher[holder.adapterPosition])
+//        }
+        holder.bind(listVoucher[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return listVoucher.size
+    }
+
+    class BoughtVoucherListHolder(private val binding: ActivityBoughtVoucherlistItemBinding, context: Context) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: ResponseVoucher) {
+            with(binding) {
+//                Glide.with(itemView.context)
+//                    .load(user.avatar_url)
+//                    .circleCrop()
+//                    .into(imgAvatar)
+                textViewVoucherListItemName.text = data.voucherName
+                textViewVoucherListItemCategory.text = data.category
+                textViewVoucherListItemPartner.text = data.partnerName
+                textViewVoucherListItemPrice.text = data.price.toString()
+            }
+        }
+    }
+}
