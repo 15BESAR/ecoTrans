98e20b296d193da473168bca2b7147b9b8857cdf
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-01 15:52:36 +0700

    small changes

--- app/src/main/java/com/android/project/ecotrans/SettingActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/SettingActivity.kt
@@ -2,8 +2,12 @@ package com.android.project.ecotrans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.project.ecotrans.databinding.ActivityProfileBinding
+import com.android.project.ecotrans.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
