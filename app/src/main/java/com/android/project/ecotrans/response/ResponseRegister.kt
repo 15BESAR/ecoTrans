a956f661f71ae87cd854d137d95e9c7ee18d757a
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-06 20:52:49 +0700

    update register response

--- app/src/main/java/com/android/project/ecotrans/response/ResponseRegister.kt
+++ app/src/main/java/com/android/project/ecotrans/response/ResponseRegister.kt
@@ -4,6 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class ResponseRegister(
 
+	@field:SerializedName("error")
+	val error: Boolean? = null,
+
 	@field:SerializedName("status")
 	val status: String? = null
 )
