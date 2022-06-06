bfe976387e70a70158c78759f6b23876d41396f2
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-06 20:52:11 +0700

    update login response

--- app/src/main/java/com/android/project/ecotrans/response/ResponseLogin.kt
+++ app/src/main/java/com/android/project/ecotrans/response/ResponseLogin.kt
@@ -4,6 +4,18 @@ import com.google.gson.annotations.SerializedName
 
 data class ResponseLogin(
 
+	@field:SerializedName("msg")
+	val msg: String? = null,
+
+	@field:SerializedName("loginResult")
+	val loginResult: LoginResult? = null,
+
+	@field:SerializedName("error")
+	val error: Boolean? = null
+)
+
+data class LoginResult(
+
 	@field:SerializedName("userId")
 	val userId: String? = null,
 
