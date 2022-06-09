e490dfc3dc07d3a73770fc63746c60d5667696c2
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-09 18:19:32 +0700

    update Response Route

--- app/src/main/java/com/android/project/ecotrans/response/ResponseRoute.kt
+++ app/src/main/java/com/android/project/ecotrans/response/ResponseRoute.kt
@@ -2,7 +2,7 @@ package com.android.project.ecotrans.response
 
 import com.google.gson.annotations.SerializedName
 
-data class ResponseRoutes(
+data class ResponseRoute(
 
 	@field:SerializedName("routes")
 	val routes: List<RoutesItem?>? = null,
@@ -14,15 +14,6 @@ data class ResponseRoutes(
 	val geocodeWaypoints: List<GeocodeWaypointsItem?>? = null
 )
 
-data class EndLocation(
-
-	@field:SerializedName("lng")
-	val lng: Double? = null,
-
-	@field:SerializedName("lat")
-	val lat: Double? = null
-)
-
 data class Northeast(
 
 	@field:SerializedName("lng")
@@ -30,66 +21,6 @@ data class Northeast(
 
 	@field:SerializedName("lat")
 	val lat: Double? = null
-)
-
-data class Duration(
-
-	@field:SerializedName("text")
-	val text: String? = null,
-
-	@field:SerializedName("value")
-	val value: Int? = null
-)
-
-data class Bounds(
-
-	@field:SerializedName("southwest")
-	val southwest: Southwest? = null,
-
-	@field:SerializedName("northeast")
-	val northeast: Northeast? = null
-)
-
-data class RoutesItem(
-
-	@field:SerializedName("reward")
-	val reward: Int? = null,
-
-	@field:SerializedName("summary")
-	val summary: String? = null,
-
-	@field:SerializedName("fare")
-	val fare: Any? = null,
-
-	@field:SerializedName("copyrights")
-	val copyrights: String? = null,
-
-	@field:SerializedName("carbon")
-	val carbon: Int? = null,
-
-	@field:SerializedName("legs")
-	val legs: List<LegsItem?>? = null,
-
-	@field:SerializedName("warnings")
-	val warnings: List<String?>? = null,
-
-	@field:SerializedName("bounds")
-	val bounds: Bounds? = null,
-
-	@field:SerializedName("overview_polyline")
-	val overviewPolyline: OverviewPolyline? = null,
-
-	@field:SerializedName("waypoint_order")
-	val waypointOrder: List<Any?>? = null
-)
-
-data class DurationInTraffic(
-
-	@field:SerializedName("text")
-	val text: String? = null,
-
-	@field:SerializedName("value")
-	val value: Int? = null
 )
 
 data class LegsItem(
@@ -137,34 +68,13 @@ data class Distance(
 	val value: Int? = null
 )
 
-data class OverviewPolyline(
-
-	@field:SerializedName("points")
-	val points: String? = null
-)
-
-data class StartLocation(
+data class EndLocation(
 
 	@field:SerializedName("lng")
 	val lng: Double? = null,
 
 	@field:SerializedName("lat")
 	val lat: Double? = null
-)
-
-data class GeocodeWaypointsItem(
-
-	@field:SerializedName("types")
-	val types: List<String?>? = null,
-
-	@field:SerializedName("geocoder_status")
-	val geocoderStatus: String? = null,
-
-	@field:SerializedName("partial_match")
-	val partialMatch: Boolean? = null,
-
-	@field:SerializedName("place_id")
-	val placeId: String? = null
 )
 
 data class StepsItem(
@@ -197,7 +107,55 @@ data class StepsItem(
 	val polyline: Polyline? = null
 )
 
+data class StartLocation(
+
+	@field:SerializedName("lng")
+	val lng: Double? = null,
+
+	@field:SerializedName("lat")
+	val lat: Double? = null
+)
+
+data class Bounds(
+
+	@field:SerializedName("southwest")
+	val southwest: Southwest? = null,
+
+	@field:SerializedName("northeast")
+	val northeast: Northeast? = null
+)
+
+data class GeocodeWaypointsItem(
+
+	@field:SerializedName("types")
+	val types: List<String?>? = null,
+
+	@field:SerializedName("geocoder_status")
+	val geocoderStatus: String? = null,
+
+	@field:SerializedName("partial_match")
+	val partialMatch: Boolean? = null,
+
+	@field:SerializedName("place_id")
+	val placeId: String? = null
+)
+
 data class Polyline(
+
+	@field:SerializedName("points")
+	val points: String? = null
+)
+
+data class Duration(
+
+	@field:SerializedName("text")
+	val text: String? = null,
+
+	@field:SerializedName("value")
+	val value: Int? = null
+)
+
+data class OverviewPolyline(
 
 	@field:SerializedName("points")
 	val points: String? = null
@@ -210,4 +168,46 @@ data class Southwest(
 
 	@field:SerializedName("lat")
 	val lat: Double? = null
+)
+
+data class RoutesItem(
+
+	@field:SerializedName("reward")
+	val reward: Int? = null,
+
+	@field:SerializedName("summary")
+	val summary: String? = null,
+
+	@field:SerializedName("fare")
+	val fare: Any? = null,
+
+	@field:SerializedName("copyrights")
+	val copyrights: String? = null,
+
+	@field:SerializedName("carbon")
+	val carbon: Int? = null,
+
+	@field:SerializedName("legs")
+	val legs: List<LegsItem?>? = null,
+
+	@field:SerializedName("warnings")
+	val warnings: List<String?>? = null,
+
+	@field:SerializedName("bounds")
+	val bounds: Bounds? = null,
+
+	@field:SerializedName("overview_polyline")
+	val overviewPolyline: OverviewPolyline? = null,
+
+	@field:SerializedName("waypoint_order")
+	val waypointOrder: List<Any?>? = null
+)
+
+data class DurationInTraffic(
+
+	@field:SerializedName("text")
+	val text: String? = null,
+
+	@field:SerializedName("value")
+	val value: Int? = null
 )
