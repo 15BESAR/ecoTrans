fed8cbfdd28d0f5b9e15c083930e4f30327627b1
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-09 18:20:14 +0700

    prepare MapNavigationViewModel.kt template (finish)

--- app/src/main/java/com/android/project/ecotrans/view_model/MapNavigationViewModel.kt
+++ app/src/main/java/com/android/project/ecotrans/view_model/MapNavigationViewModel.kt
@@ -8,25 +8,26 @@ import com.android.project.ecotrans.model.UserModel
 import com.android.project.ecotrans.model.UserPreference
 import com.android.project.ecotrans.response.PredictionsItem
 import com.android.project.ecotrans.response.ResponseAutoComplete
+import com.android.project.ecotrans.response.ResponseForecast
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
-import retrofit2.Response
 
 class MapNavigationViewModel(private val pref: UserPreference) : ViewModel() {
-
-    private var _isLoadingPreferenceList = MutableLiveData<Boolean>()
-    var isLoadingPreferenceList: LiveData<Boolean> = _isLoadingPreferenceList
-
-    private var _isLoadingJourneyInformation = MutableLiveData<Boolean>()
-    var isLoadingJourneyInformation: LiveData<Boolean> = _isLoadingJourneyInformation
 
     private var _isLoading = MutableLiveData<Boolean>()
     var isLoading: LiveData<Boolean> = _isLoading
 
+    private var _temp = MutableLiveData<Double>()
+    var temp: LiveData<Double> = _temp
+    private var _uv = MutableLiveData<Double>()
+    var uv: LiveData<Double> = _uv
+    private var _aqi = MutableLiveData<Double>()
+    var aqi: LiveData<Double> = _aqi
 
     private var _isError = MutableLiveData<Boolean>()
     var isError: LiveData<Boolean> = _isError
@@ -40,37 +41,36 @@ class MapNavigationViewModel(private val pref: UserPreference) : ViewModel() {
     init {
     }
 
-//    fun getUserData(token: String, id: String) {
-//        _isLoadingDasboard.value = true
-//        _isError.value = false
-//        _isDetailed.value = true
-//
-//        var client = ApiConfig.getApiService().user("Bearer $token", id)
-//        client.enqueue(object : Callback<User> {
-//            override fun onResponse(
-//                call: Call<User>,
-//                response: retrofit2.Response<User>
-//            ) {
-//                _isLoadingDasboard.value = false
-//                if (response.isSuccessful) {
-//                    _userData.value = response.body() as User
-//                    if(_userData.value?.job.isNullOrEmpty()){
-//                        _isDetailed.value = false
-//                    }
-//
-//                } else {
-//                    Log.e("MainActivity", "onFailure: ${response.message()}")
-//
-////                    _errorMessage.value = "Wrong Password or Email"
-////                    _isError.value = true
-//                }
-//            }
-//            override fun onFailure(call: Call<User>, t: Throwable) {
-////                _isLoading.value = false
-////                _isError.value = true
-////                _errorMessage.value = t.message as String
-//                Log.e("MainActivity", "onFailure: ${t.message}")
-//            }
-//        })
-//    }
+    fun getForecastData(token: String, requestBody: RequestBody) {
+        _isLoading.value = true
+        _isError.value = false
+
+        var client = ApiConfig.getApiService().getForecast("Bearer $token", requestBody)
+        client.enqueue(object : Callback<ResponseForecast> {
+            override fun onResponse(
+                call: Call<ResponseForecast>,
+                response: retrofit2.Response<ResponseForecast>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+
+                    _aqi.value = response.body()?.forecast?.aqi as Double
+                    _temp.value = response.body()?.forecast?.temp as Double
+                    _uv.value = response.body()?.forecast?.uv as Double
+
+                } else {
+                    Log.e("MainActivity", "onFailure: ${response.message()}")
+
+//                    _errorMessage.value = "Wrong Password or Email"
+//                    _isError.value = true
+                }
+            }
+            override fun onFailure(call: Call<ResponseForecast>, t: Throwable) {
+//                _isLoading.value = false
+//                _isError.value = true
+//                _errorMessage.value = t.message as String
+                Log.e("MainActivity", "onFailure: ${t.message}")
+            }
+        })
+    }
 }
