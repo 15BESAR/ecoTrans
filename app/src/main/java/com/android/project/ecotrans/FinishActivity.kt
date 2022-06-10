c0669ce4f2e01d9feba71a47ab99bdb232a841ba
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-10 15:48:29 +0700

    FinishActivity.kt finish with temp parameter

--- app/src/main/java/com/android/project/ecotrans/FinishActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/FinishActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -11,11 +12,13 @@ import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import com.android.project.ecotrans.databinding.ActivityFinishBinding
 import com.android.project.ecotrans.model.UserPreference
+import com.android.project.ecotrans.response.ResponseFinish
 import com.android.project.ecotrans.view_model.FinishViewModel
 import com.android.project.ecotrans.view_model.ViewModelFactory
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
+import kotlin.properties.Delegates
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -23,10 +26,25 @@ class FinishActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFinishBinding
     private lateinit var finishViewModel: FinishViewModel
 
+    private lateinit var token: String
+    private lateinit var id: String
+
+    private lateinit var destinationId: String
+    private lateinit var originId: String
+    private var distance by Delegates.notNull<Int>()
+    private var carbon by Delegates.notNull<Int>()
+    private var reward by Delegates.notNull<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFinishBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        this.originId = intent.getStringExtra("originId") as String
+        this.destinationId = intent.getStringExtra("destinationId") as String
+        this.distance = intent.getIntExtra("distance", 0)
+        this.carbon = intent.getIntExtra("carbon", 0)
+        this.reward = intent.getIntExtra("reward", 0)
 
         setupViewModel()
         setupView()
@@ -52,28 +70,50 @@ class FinishActivity : AppCompatActivity() {
             showErrorMessage(it)
         }
 
-        //finish journey
-        val json = JSONObject()
-        json.put("origin", "ChIJl02Bz3GMaS4RCgefgFZdKtI")
-        json.put("destination", "ChIJY9TrwiH0aS4RrvGqlZvI_Mw")
-        json.put("startTime", "2018-12-10T13:49:51.141Z")
-        json.put("finishTime", "2018-12-10T16:49:51.141Z")
-        json.put("distanceTravelled", 10.43F)
-//        json.put("emissionSaved", 4.45F)
-//        json.put("reward", 102)
-        val requestBody = json.toString().toRequestBody("application/json".toMediaTypeOrNull())
+        finishViewModel.getUser().observe(this){
+            if (it != null){
+                this.id = it.id
+                this.token = it.token
 
+                //finish journey
+                val json = JSONObject()
+                json.put("userId", "37778226-035e-417c-8baf-509e888657f2")
+                json.put("origin", "ChIJl02Bz3GMaS4RCgefgFZdKtI")
+                json.put("destination", "ChIJY9TrwiH0aS4RrvGqlZvI_Mw")
+                json.put("startTime", "2018-12-10T13:49:51.141Z")
+                json.put("finishTime", "2018-12-10T16:49:51.141Z")
+                json.put("distanceTravelled", 10.43F)
+                json.put("emissionSaved", 4.45F)
+                json.put("reward", 102)
+                val requestBody = json.toString().toRequestBody("application/json".toMediaTypeOrNull())
+
+                finishViewModel.postJourney(this.token, requestBody)
+            }
+        }
+
+        finishViewModel.finishData.observe(this){
+            if(it != null){
+                setupViewFinish(it)
+            }
+        }
+
+
+
+
+    }
+
+    private fun setupViewFinish(it: ResponseFinish) {
+        binding.textViewFinishReward.text = it.reward.toString()
+        binding.textViewFinishEmissionSaved.text = it.emissionSaved.toString()
+        binding.textViewFinishDistanceTraveled.text = it.distanceTravelled.toString()
+        binding.textViewFinishJourneyId.text = it.journeyId.toString()
+        binding.textViewFinishTimeStart.text = it.finishTime.toString()
+        binding.textViewFinishTimeFinish.text = it.startTime.toString()
 
     }
 
     private fun setupView() {
         supportActionBar?.hide()
-
-        binding.textViewFinishReward.text = "100 Given"
-        binding.textViewFinishEmissionSaved.text = "42.01"
-        binding.textViewFinishDistanceTraveled.text = "2.1 km"
-        binding.textViewFinishTimeFinish.text = "16:00"
-        binding.textViewFinishTimeStart.text = "15:00"
     }
 
     private fun setupAction() {
@@ -83,15 +123,23 @@ class FinishActivity : AppCompatActivity() {
     }
 
     private fun showLoading(it: Boolean) {
-//        if (isLoading) {
-//            binding.progressBarLogin.visibility = View.VISIBLE
-//            binding.btnLogin.visibility = View.GONE
-//            binding.textGoToSignUp.visibility = View.GONE
-//        } else {
-//            binding.progressBarLogin.visibility = View.GONE
-//            binding.btnLogin.visibility = View.VISIBLE
-//            binding.textGoToSignUp.visibility = View.VISIBLE
-//        }
+        if (it) {
+            binding.progressBarFinish.visibility = View.VISIBLE
+            binding.textViewFinishReward.visibility = View.GONE
+            binding.textViewFinishEmissionSaved.visibility = View.GONE
+            binding.textViewFinishDistanceTraveled.visibility = View.GONE
+            binding.textViewFinishJourneyId.visibility = View.GONE
+            binding.textViewFinishTimeStart.visibility = View.GONE
+            binding.textViewFinishTimeFinish.visibility = View.GONE
+        } else {
+            binding.progressBarFinish.visibility = View.GONE
+            binding.textViewFinishReward.visibility = View.VISIBLE
+            binding.textViewFinishEmissionSaved.visibility = View.VISIBLE
+            binding.textViewFinishDistanceTraveled.visibility = View.VISIBLE
+            binding.textViewFinishJourneyId.visibility = View.VISIBLE
+            binding.textViewFinishTimeStart.visibility = View.VISIBLE
+            binding.textViewFinishTimeFinish.visibility = View.VISIBLE
+        }
     }
 
     private fun showErrorMessage(errorMessage: String){
--- app/src/main/java/com/android/project/ecotrans/view_model/FinishViewModel.kt
+++ app/src/main/java/com/android/project/ecotrans/view_model/FinishViewModel.kt
@@ -1,9 +1,15 @@
 package com.android.project.ecotrans.view_model
 
+import android.util.Log
 import androidx.lifecycle.*
+import com.android.project.ecotrans.api_config.ApiConfig
 import com.android.project.ecotrans.model.UserModel
 import com.android.project.ecotrans.model.UserPreference
+import com.android.project.ecotrans.response.ResponseFinish
 import kotlinx.coroutines.launch
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
 
 class FinishViewModel(private val pref: UserPreference) : ViewModel() {
 
@@ -16,6 +22,9 @@ class FinishViewModel(private val pref: UserPreference) : ViewModel() {
     private var _errorMessage = MutableLiveData<String>()
     var errorMessage: LiveData<String> = _errorMessage
 
+    private var _finishData = MutableLiveData<ResponseFinish>()
+    var finishData: LiveData<ResponseFinish> = _finishData
+
     fun getUser(): LiveData<UserModel> {
         return pref.getUser().asLiveData()
     }
@@ -23,39 +32,32 @@ class FinishViewModel(private val pref: UserPreference) : ViewModel() {
     init {
     }
 
-//    fun postUpdate(email: String, password: String) {
-//        _isLoading.value = true
-//        _isError.value = false
-//        var userModel: UserModel
-//        var client = ApiConfig.getApiService().login(email, password)
-//        client.enqueue(object : Callback<PostResponseLogin> {
-//            override fun onResponse(
-//                call: Call<PostResponseLogin>,
-//                response: retrofit2.Response<PostResponseLogin>
-//            ) {
-//                _isLoading.value = false
-//                if (response.isSuccessful) {
-//                    var id = response.body()?.loginResult?.userId as String
-//                    var name = response.body()?.loginResult?.name as String
-//                    var isLogin = true
-//                    var token = response.body()?.loginResult?.token as String
-//                    userModel = UserModel(id, name, isLogin, token)
-//
-//                    _errorMessage.value = "login " + response.body()?.message as String
-//                    saveUser(userModel)
-//                } else {
-//                    Log.e("MainActivity", "onFailure: ${response.message()}")
-//
-//                    _errorMessage.value = "Wrong Password or Email"
-//                    _isError.value = true
-//                }
-//            }
-//            override fun onFailure(call: Call<PostResponseLogin>, t: Throwable) {
-//                _isLoading.value = false
-//                _isError.value = true
-//                _errorMessage.value = t.message as String
-//                Log.e("MainActivity", "onFailure: ${t.message}")
-//            }
-//        })
-//    }
+    fun postJourney(token: String, requestBody: RequestBody) {
+        _isLoading.value = true
+        _isError.value = false
+
+        var client = ApiConfig.getApiService().finishJourney("Bearer $token", requestBody)
+        client.enqueue(object : Callback<ResponseFinish> {
+            override fun onResponse(
+                call: Call<ResponseFinish>,
+                response: retrofit2.Response<ResponseFinish>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _finishData.value = response.body() as ResponseFinish
+                } else {
+                    Log.e("MainActivity", "onFailure: ${response.message()}")
+
+                    _errorMessage.value = response.message()
+                    _isError.value = true
+                }
+            }
+            override fun onFailure(call: Call<ResponseFinish>, t: Throwable) {
+
+                _isError.value = true
+                _errorMessage.value = t.message as String
+                Log.e("MainActivity", "onFailure: ${t.message}")
+            }
+        })
+    }
 }
