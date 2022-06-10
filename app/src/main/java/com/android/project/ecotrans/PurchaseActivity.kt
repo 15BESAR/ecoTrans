d0a5a0d420d716f12825690d074faeba796c29ca
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-10 15:39:02 +0700

    purchase finish

--- app/src/main/java/com/android/project/ecotrans/PurchaseActivity.kt
+++ app/src/main/java/com/android/project/ecotrans/PurchaseActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -11,10 +12,9 @@ import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.android.project.ecotrans.databinding.ActivityMainBinding
 import com.android.project.ecotrans.databinding.ActivityPurchaseBinding
 import com.android.project.ecotrans.model.UserPreference
-import com.android.project.ecotrans.response.ResponseVoucher
+import com.android.project.ecotrans.response.VouchersItem
 import com.android.project.ecotrans.view_model.PurchaseViewModel
 import com.android.project.ecotrans.view_model.ViewModelFactory
 
@@ -25,6 +25,8 @@ class PurchaseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPurchaseBinding
     private lateinit var purchaseViewModel: PurchaseViewModel
     private lateinit var userId: String
+    private lateinit var token: String
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,10 +35,10 @@ class PurchaseActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val isDetailed:Boolean = intent.getBooleanExtra("isDetailed", false)
-
+        val points:Int = intent.getIntExtra("points", 0)
 
         setupViewModel(isDetailed)
-        setupView()
+        setupView(points)
         setupAction()
 //        setupAnimation()
     }
@@ -56,6 +58,10 @@ class PurchaseActivity : AppCompatActivity() {
             }
         }
 
+        purchaseViewModel.isLoading.observe(this){
+            showLoading(it)
+        }
+
         purchaseViewModel.errorMessage.observe(this){
             showErrorMessage(it)
         }
@@ -70,18 +76,29 @@ class PurchaseActivity : AppCompatActivity() {
         purchaseViewModel.getUser().observe(this){
             if(!it.id.isNullOrEmpty()){
                 this.userId = it.id
+                this.token = it.token
+                purchaseViewModel.getAllVoucher(this.token)
+            }
+        }
+
+        purchaseViewModel.vouchers.observe(this){
+            if(it != null){
+                setupVoucherList(it)
             }
         }
     }
 
-    private fun setupView() {
+    private fun setupView(points: Int) {
         supportActionBar?.hide()
+
+        binding.textViewPurchasePointsValue.text = points.toString()
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerViewPurchaseVoucherlist.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.recyclerViewPurchaseVoucherlist.addItemDecoration(itemDecoration)
-        setupVoucherList()
+
+//        setupVoucherList()
     }
 
     private fun setupAction() {
@@ -94,9 +111,9 @@ class PurchaseActivity : AppCompatActivity() {
 //        TODO("Not yet implemented")
 //    }
 
-    private fun setupVoucherList(){
-        val listVoucher = ArrayList<ResponseVoucher>()
-        var voucher : ResponseVoucher
+    private fun setupVoucherList(list: List<VouchersItem>) {
+//        val listVoucher = ArrayList<ResponseVoucher>()
+//        var voucher : ResponseVoucher
 //        for (item in items!!){
 //            user = User()
 //
@@ -111,59 +128,60 @@ class PurchaseActivity : AppCompatActivity() {
 //            listItem.add(user)
 //        }
 
-        voucher = ResponseVoucher()
-        voucher.category = "GoFood"
-        voucher.partnerName = "GOJEK"
-        voucher.price = 500
-        voucher.stock = 10
-        voucher.voucherName = "Diskon 50% hingga 25RB"
-        listVoucher.add(voucher)
-
-        voucher = ResponseVoucher()
-        voucher.category = "Furniture"
-        voucher.partnerName = "Tokopedia"
-        voucher.price = 1000
-        voucher.stock = 10
-        voucher.voucherName = "Diskon 5% hingga 100RB"
-        listVoucher.add(voucher)
-
-        voucher = ResponseVoucher()
-        voucher.category = "GoSend"
-        voucher.partnerName = "GOJEK"
-        voucher.price = 200
-        voucher.stock = 10
-        voucher.voucherName = "Gratis Ongkir hingga 10RB"
-        listVoucher.add(voucher)
-
-        voucher = ResponseVoucher()
-        voucher.category = "GoFood"
-        voucher.partnerName = "GOJEK"
-        voucher.price = 500
-        voucher.stock = 10
-        voucher.voucherName = "Diskon 50% hingga 25RB"
-        listVoucher.add(voucher)
-
-        voucher = ResponseVoucher()
-        voucher.category = "Furniture"
-        voucher.partnerName = "Tokopedia"
-        voucher.price = 1000
-        voucher.stock = 10
-        voucher.voucherName = "Diskon 5% hingga 100RB"
-        listVoucher.add(voucher)
-
-        voucher = ResponseVoucher()
-        voucher.category = "GoSend"
-        voucher.partnerName = "GOJEK"
-        voucher.price = 200
-        voucher.stock = 10
-        voucher.voucherName = "Gratis Ongkir hingga 10RB"
-        listVoucher.add(voucher)
+//        voucher = ResponseVoucher()
+//        voucher.category = "GoFood"
+//        voucher.partnerName = "GOJEK"
+//        voucher.price = 500
+//        voucher.stock = 10
+//        voucher.voucherName = "Diskon 50% hingga 25RB"
+//        listVoucher.add(voucher)
+//
+//        voucher = ResponseVoucher()
+//        voucher.category = "Furniture"
+//        voucher.partnerName = "Tokopedia"
+//        voucher.price = 1000
+//        voucher.stock = 10
+//        voucher.voucherName = "Diskon 5% hingga 100RB"
+//        listVoucher.add(voucher)
+//
+//        voucher = ResponseVoucher()
+//        voucher.category = "GoSend"
+//        voucher.partnerName = "GOJEK"
+//        voucher.price = 200
+//        voucher.stock = 10
+//        voucher.voucherName = "Gratis Ongkir hingga 10RB"
+//        listVoucher.add(voucher)
+//
+//        voucher = ResponseVoucher()
+//        voucher.category = "GoFood"
+//        voucher.partnerName = "GOJEK"
+//        voucher.price = 500
+//        voucher.stock = 10
+//        voucher.voucherName = "Diskon 50% hingga 25RB"
+//        listVoucher.add(voucher)
+//
+//        voucher = ResponseVoucher()
+//        voucher.category = "Furniture"
+//        voucher.partnerName = "Tokopedia"
+//        voucher.price = 1000
+//        voucher.stock = 10
+//        voucher.voucherName = "Diskon 5% hingga 100RB"
+//        listVoucher.add(voucher)
+//
+//        voucher = ResponseVoucher()
+//        voucher.category = "GoSend"
+//        voucher.partnerName = "GOJEK"
+//        voucher.price = 200
+//        voucher.stock = 10
+//        voucher.voucherName = "Gratis Ongkir hingga 10RB"
+//        listVoucher.add(voucher)
 
         val adapter = PurchaseVoucherListAdapter()
         adapter.setContext(this)
-        adapter.setListVoucher(listVoucher)
+        adapter.setListVoucher(list)
         adapter.setViewModel(purchaseViewModel)
-//        adapter.setUserId(this.userId)
+        adapter.setUserId(this.userId)
+        adapter.setToken(this.token)
         binding.recyclerViewPurchaseVoucherlist.adapter = adapter
 
 //        adapter.setOnItemClickCallback(object : PurchaseVoucherListAdapter.OnItemClickback{
@@ -176,6 +194,16 @@ class PurchaseActivity : AppCompatActivity() {
 //    private fun buyVoucher(data: ResponseVoucher) {
 //        //
 //    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBarPurchaseVoucherlist.visibility = View.VISIBLE
+            binding.recyclerViewPurchaseVoucherlist.visibility = View.GONE
+        } else {
+            binding.progressBarPurchaseVoucherlist.visibility = View.GONE
+            binding.recyclerViewPurchaseVoucherlist.visibility = View.VISIBLE
+        }
+    }
 
     private fun showErrorMessage(errorMessage: String){
         Toast.makeText(this@PurchaseActivity, errorMessage.toString(), Toast.LENGTH_SHORT).show()
