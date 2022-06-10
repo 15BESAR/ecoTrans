2e3f82ba64e5b87a8482ab95e930f21e6ce8c5fd
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2022-06-10 15:38:04 +0700

    purchase view model finish

--- app/src/main/java/com/android/project/ecotrans/view_model/PurchaseViewModel.kt
+++ app/src/main/java/com/android/project/ecotrans/view_model/PurchaseViewModel.kt
@@ -1,9 +1,18 @@
 package com.android.project.ecotrans.view_model
 
+import android.util.Log
 import androidx.lifecycle.*
+import com.android.project.ecotrans.api_config.ApiConfig
 import com.android.project.ecotrans.model.UserModel
 import com.android.project.ecotrans.model.UserPreference
+import com.android.project.ecotrans.response.ResponseGetAllVoucher
+import com.android.project.ecotrans.response.ResponsePurchaseVoucher
+import com.android.project.ecotrans.response.VouchersItem
 import kotlinx.coroutines.launch
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import java.util.ArrayList
 
 class PurchaseViewModel(private val pref: UserPreference) : ViewModel() {
 
@@ -19,6 +28,9 @@ class PurchaseViewModel(private val pref: UserPreference) : ViewModel() {
     private var _isBought = MutableLiveData<Boolean>()
     var isBought: LiveData<Boolean> = _isBought
 
+    private var _vouchers = MutableLiveData<List<VouchersItem>>()
+    var vouchers: LiveData<List<VouchersItem>> = _vouchers
+
     fun getUser(): LiveData<UserModel> {
         return pref.getUser().asLiveData()
     }
@@ -26,44 +38,62 @@ class PurchaseViewModel(private val pref: UserPreference) : ViewModel() {
     init {
     }
 
-    fun test(s: String){
-        _errorMessage.value = "$s voucher bought"
-        _isBought.value = true
-    }
+    fun getAllVoucher(token: String) {
+        _isLoading.value = true
+        _isError.value = false
+        var client = ApiConfig.getApiService().getAllVoucher("Bearer $token")
+        client.enqueue(object : Callback<ResponseGetAllVoucher> {
+            override fun onResponse(
+                call: Call<ResponseGetAllVoucher>,
+                response: retrofit2.Response<ResponseGetAllVoucher>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _vouchers.value = response.body()?.vouchers as List<VouchersItem>
 
-    fun postUpdate(email: String, password: String) {
-//        _isLoading.value = true
-//        _isError.value = false
-//        var userModel: UserModel
-//        var client = ApiConfig.getApiService().login(email, password)
-//        client.enqueue(object : Callback<PostResponseLogin> {
-//            override fun onResponse(
-//                call: Call<PostResponseLogin>,
-//                response: retrofit2.Response<PostResponseLogin>
-//            ) {
-//                _isLoading.value = false
-//                if (response.isSuccessful) {
-//                    var id = response.body()?.loginResult?.userId as String
-//                    var name = response.body()?.loginResult?.name as String
-//                    var isLogin = true
-//                    var token = response.body()?.loginResult?.token as String
-//                    userModel = UserModel(id, name, isLogin, token)
-//
-//                    _errorMessage.value = "login " + response.body()?.message as String
-//                    saveUser(userModel)
-//                } else {
-//                    Log.e("MainActivity", "onFailure: ${response.message()}")
-//
+                } else {
+                    Log.e("MainActivity", "onFailure: ${response.message()}")
+
 //                    _errorMessage.value = "Wrong Password or Email"
 //                    _isError.value = true
-//                }
-//            }
-//            override fun onFailure(call: Call<PostResponseLogin>, t: Throwable) {
+                }
+            }
+            override fun onFailure(call: Call<ResponseGetAllVoucher>, t: Throwable) {
 //                _isLoading.value = false
 //                _isError.value = true
 //                _errorMessage.value = t.message as String
-//                Log.e("MainActivity", "onFailure: ${t.message}")
-//            }
-//        })
+                Log.e("MainActivity", "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    fun purchaseVoucher(token: String, requestBody: RequestBody, voucherName: String) {
+        _isLoading.value = true
+        _isError.value = false
+        var client = ApiConfig.getApiService().purchase("Bearer $token", requestBody)
+        client.enqueue(object : Callback<ResponsePurchaseVoucher> {
+            override fun onResponse(
+                call: Call<ResponsePurchaseVoucher>,
+                response: retrofit2.Response<ResponsePurchaseVoucher>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _errorMessage.value = "$voucherName voucher bought"
+                    _isBought.value = true
+
+                } else {
+                    Log.e("MainActivity", "onFailure: ${response.message()}")
+
+                    _errorMessage.value = "User points is not enough!"
+//                    _isError.value = true
+                }
+            }
+            override fun onFailure(call: Call<ResponsePurchaseVoucher>, t: Throwable) {
+//                _isLoading.value = false
+//                _isError.value = true
+                _errorMessage.value = "User points is not enough!"
+                Log.e("MainActivity", "onFailure: ${t.message}")
+            }
+        })
     }
 }
